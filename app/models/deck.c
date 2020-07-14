91977e900ec773d74c34340bb5fc63ab287e4a5f
Iacopo Melani <iacopo.melani@madeinapp.net>
2020-07-14 17:13:27 +0200

    Implementato shuffle di deck.h

--- app/models/deck.c
+++ app/models/deck.c
@@ -6,6 +6,8 @@
 #include "../utils/utils.h"
 #include "../../libs/mmalloc/alloc/mmalloc.h"
 
+#define SHUFFLE_TIMES 5
+
 struct Card **deck;
 
 static void deck_init_cards(struct Card **deck);
@@ -15,7 +17,7 @@ void deck_init()
     deck = mmalloc(DECK_LEN * sizeof(struct Card), "deck");
     deck_init_cards(deck);
 
-    shuffle_array(deck, DECK_LEN, sizeof(deck[0]));
+    deck_shuffle();
 
     for (int i = 0; i < DECK_LEN; i++)
     {
@@ -23,6 +25,14 @@ void deck_init()
         mfree(deck[i], "card");
     }
     mfree(deck, "deck");
+}
+
+void deck_shuffle()
+{
+    for (int i = 0; i < SHUFFLE_TIMES; i++)
+    {
+        shuffle_array(deck, DECK_LEN, sizeof(deck[0]));
+    }
 }
 
 static void deck_init_cards(struct Card **d)
